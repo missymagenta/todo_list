c2ded12f1c433e422d6e9d43bf07400aceace164
missymagenta <alina.soare37@gmail.com>
2019-06-03 20:26:32 +0200

    first commit

---
+++ todo_list/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html', {})
+
+def about(request):
+    my_name = 'John Elder'
+    return render(request, 'about.html', {'name': my_name})
