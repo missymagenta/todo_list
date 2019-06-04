17483029245689ff99258b26b695cf61b99fe3e7
missymagenta <alina.soare37@gmail.com>
2019-06-04 16:04:57 +0200

    final commit

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -19,4 +19,8 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('about/',views.about, name='about'),
+    path('delete/<list_id>',views.delete, name='delete'),
+    path('done/<list_id>',views.done, name='done'),
+    path('undone/<list_id>',views.undone, name='undone'),
+    path('edit/<list_id>',views.edit, name='edit'),
 ]
